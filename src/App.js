f89626022e54466d141f715d4fe06c849eb93cb8
Tega Oke <oketegah@gmail.com>
2019-06-20 14:05:17 +0100

    Routed home Link to home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Navbar from './Navbar/Navabar';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
+
+import './App.css';
 
 function App() {
   const [displayState, switchDisplayState] = useState({});
@@ -10,7 +13,10 @@ function App() {
   return (
     <>
       <Navbar />
-      <Header />
+      <Router>
+        <Route path="/" exact component={Header} />
+      </Router>
+      {/* <Header /> */}
       <Footer />
     </>
   );
--- src/Navbar/Navabar.js
+++ src/Navbar/Navabar.js
@@ -1,30 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
 import profileImage from '../images/profile.jpeg';
 import './Navbar.css';
 
 const navbar = props => {
   return (
-    <nav className="navbar">
-      <div className="navbar-item">
-        <img src={profileImage} alt={'profile'} />
-      </div>
-      <ul className="navbar-item">
-        <a className="menu-item active" href="./index.html">
-          <li>HOME</li>
-        </a>
-        <a className="menu-item" href="./blog.html">
-          <li>BLOG</li>
-        </a>
-        <a className="menu-item" href="./contact.html">
-          <li>CONTACT</li>
-        </a>
-      </ul>
-      <div className="navbar-item">
-        <a className="hireLink" href="contact.html">
-          Hire me
-        </a>
-      </div>
-    </nav>
+    <Router>
+      <nav className="navbar">
+        <div className="navbar-item">
+          <img src={profileImage} alt={'profile'} />
+        </div>
+        <ul className="navbar-item">
+          <Link className="menu-item active" to="/home">
+            <li>HOME</li>
+          </Link>
+          <a className="menu-item" href="./blog.html">
+            <li>BLOG</li>
+          </a>
+          <a className="menu-item" href="./contact.html">
+            <li>CONTACT</li>
+          </a>
+        </ul>
+        <div className="navbar-item">
+          <a className="hireLink" href="contact.html">
+            Hire me
+          </a>
+        </div>
+      </nav>
+    </Router>
   );
 };
 
