413152055da62a9b5ea49d5671fd30a1070281b6
Tega Oke <oketegah@gmail.com>
2019-06-20 13:42:38 +0100

    Created Footer component and added to main App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { useState } from 'react';
 import './App.css';
 import Navbar from './Navbar/Navabar';
-import Display from './Display/Display';
 import Header from './Header/Header';
 
 function App() {
-  const [displayState, switchDisplayState] = useState({
-    
-  });
+  const [displayState, switchDisplayState] = useState({});
 
   return (
-    <Display>
+    <>
       <Navbar />
       <Header />
-    </Display>
+    </>
   );
 }
 
---
+++ src/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = props => {
+  return (
+    <footer>
+      <p>Copyright &copy; Tega</p>
+    </footer>
+  );
+};
+export default Footer;
