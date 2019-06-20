db43709f8b893dd92adf09500f06acf0cad70e26
Tega Oke <oketegah@gmail.com>
2019-06-20 08:33:19 +0100

    Included navbar components to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Navbar/Navabar';
 
 function App() {
-  return <div />;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
