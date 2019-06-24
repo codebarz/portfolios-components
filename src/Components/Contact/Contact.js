637c6bb0b55748428b50a7899d39406c44ba2bc6
Tega Oke <oketegah@gmail.com>
2019-06-24 06:30:09 +0100

    Added preview slate for contact form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Contact.css';
 
 const Contact = props => {
+  const [fullnameState, changeFullnameState] = useState('');
+
+  const changeFullnameStateHandler = event => {
+    const fullnamePreview = document.querySelector('.blog-aside .fullname');
+    fullnamePreview.innerHTML = event.target.value;
+    changeFullnameState(event.target.value);
+  };
   return (
     <main className="hire-main">
       <article>
@@ -16,13 +23,16 @@ const Contact = props => {
             type="text"
             name="fullname"
             id="fullname"
+            value={fullnameState}
             placeholder="Full Name"
+            onChange={changeFullnameStateHandler}
             required
           />
           <input
             type="email"
             name="email"
             id="email"
+            // value={inputState.inputsValue[1].email}
             placeholder="Email Address"
             required
           />
@@ -30,6 +40,7 @@ const Contact = props => {
             type="number"
             name="phonenumber"
             id="phonenumber"
+            // value={inputState.inputsValue[2].phoneNumber}
             placeholder="Phone Number"
           />
           <textarea
@@ -37,18 +48,26 @@ const Contact = props => {
             id="project"
             placeholder="What's your project about"
             rows="10"
+            // value={inputState.inputsValue[3].project}
             required
           />
           <br />
           <br />
-          <label for="submit">
-            <i class="mdi mdi-send" />
+          <label htmlFor="submit">
+            <i className="mdi mdi-send" />
             Send
           </label>
           <input type="submit" name="submit" id="submit" value="Send" />
         </form>
       </article>
-      <aside />
+      <aside className="blog-aside">
+        <div className="contact-preview">
+          <p className="fullname" />
+          <p className="email" />
+          <p className="phoneNumber" />
+          <p className="project" />
+        </div>
+      </aside>
     </main>
   );
 };
